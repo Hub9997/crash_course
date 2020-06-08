d9b4654db68f54e26eb0078586b0a73d0a949213
Hub9997 <hrubetex@gmail.com>
2020-06-08 16:03:05 +0200

    iczenia ze str 66-111

--- pizzas_and_more.py
+++ pizzas_and_more.py
@@ -19,8 +19,10 @@ print(friend_warzywas)
 
 friend_warzywas.append('pomidor')
 print(friend_warzywas)
-
 print("My favorite warzywas are : "  )
+
+
+
 moje = [warz for warz in warzywa]
 print(moje)
 
@@ -28,3 +30,47 @@ print("And my friend likes : ")
 jego = [warz for warz in friend_warzywas]
 print(jego)
 #write two for loops to print each list of food
+
+miejsca = ['Hiszpania','Wlochy','Majorka','Tybet','Tajlandia']
+print(miejsca)
+print(sorted(miejsca))
+print(miejsca)
+miejsca.reverse()
+print(miejsca)
+miejsca.reverse()
+print(miejsca)
+miejsca.sort()
+print(miejsca)
+
+guests = ['Lukasz', 'Agnieszka','Mariola']
+print(guests[0] +" I'm inviting You to a dinner.")
+print(guests[1] +" I'm inviting You to a dinner.")
+print(guests[2] +" I'm inviting You to a dinner.")
+print(guests[1]+ " can't make it.")
+guests[1] = 'Vardan'
+print(len(guests))
+
+print(guests[0] +" I'm inviting You to a dinner.")
+print(guests[1] +" I'm inviting You to a dinner.")
+print(guests[2] +" I'm inviting You to a dinner.")
+print(" I've found a bigger table.")
+guests.insert(1,'Ewelina')
+print(guests)
+guests.insert(2,'Ania')
+print(guests[0] +" I'm inviting You to a dinner.")
+print(guests[1] +" I'm inviting You to a dinner.")
+print(guests)
+guests.append('Kasia')
+print(guests)
+guests.pop()
+print(guests)
+guests.pop()
+print(guests)
+guests.pop()
+print(guests)
+guests.pop(0)
+print(guests)
+del guests[0]
+print(guests)
+del guests[0]
+print(guests)
